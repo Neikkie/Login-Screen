64c31082f3179f70516654ae97c3618d4cebe3a2
Neikkie <neikkie21@outlook.com>
2025-09-05 17:15:59 -0400

    Initial Commit

---
+++ Login Screen/Login_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_ScreenApp.swift
+//  Login Screen
+//
+//  Created by Shanique Beckford on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Login_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
